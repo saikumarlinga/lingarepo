9a22a9066a5672aa7f385223855fe444541c64d1
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-23 17:58:01 +0530

    done

--- src/StashProject.java
+++ src/StashProject.java
@@ -4,6 +4,8 @@ public class StashProject {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world2");
+		System.out.println("  made changes in sai ");
+		System.out.println("hello world 3...!");
 
 	}
 
